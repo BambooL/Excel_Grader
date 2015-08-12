e82e79472e74d39e1b5f48db1f38a73459740eee
BambooL <bambool@psu.edu>
2015-08-12 17:39:36 -0400

    2 Tab implemented

---
+++ src/test/Generate.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Generate {
+	private String assignGenField;
+	private String assignTemField;
+	private String nameField;
+	public void Generate (String assignGenField, String assignTemField, String nameField) {
+		
+	}
+}
--- src/test/UserInterface.java
+++ src/test/UserInterface.java
@@ -4,8 +4,10 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,18 +19,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
@@ -40,7 +44,7 @@ import test.Test;
 
 public class UserInterface extends JFrame {
 
-	private JPanel panel;
+	private JPanel panel = new JPanel();
 	private JTextArea area;
     public UserInterface() {
         
@@ -49,15 +53,94 @@ public class UserInterface extends JFrame {
 
     public void initUI() {
     	panel = (JPanel) getContentPane();
+    	
+    	JTabbedPane tabbedPane = new JTabbedPane();
+    	JComponent panel1 = makeTextPanel("");
+    	tabbedPane.addTab("Tab 1", null, panel1,
+                "Does nothing");
+    	tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+         
+        JComponent panel2 = makeTextPanel("");
+        tabbedPane.addTab("Tab 2", null, panel2,
+                "Does twice as much nothing");
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        
+        
+//     panel1 layout
+        panel1.setLayout(new FlowLayout());
+        
+        JButton assignGenbtn = new JButton("Browse Assignment Path");
+        JButton assignTembtn = new JButton("Browse Assignment Template");
+        JButton namebtn = new JButton("Browse Name List");
+        JButton generatebtn = new JButton("Generate");
+        final JTextField assignGenField = new JTextField();
+        final JTextField assignTemField = new JTextField();
+        final JTextField nameField = new JTextField();
+        nameField.setPreferredSize(new Dimension(320, 20));
+        assignGenField.setPreferredSize(new Dimension(320, 20));
+        assignTemField.setPreferredSize(new Dimension(320, 20));
+        panel1.add(assignGenbtn);
+        panel1.add(assignGenField);
+        panel1.add(assignTembtn);
+        panel1.add(assignTemField);  
+        panel1.add(namebtn);
+        panel1.add(nameField);
+        panel1.add(generatebtn);
+//      panel 1     
+    	
+	  	assignGenbtn.addActionListener(new ActionListener() {
+	      	
+	      	@Override
+	      	public void actionPerformed(ActionEvent e) {
+	      		// TODO Auto-generated method stub
+	      		String path = null;
+	      		
+	  	        JFileChooser fileChooser = new JFileChooser(".");
+	  	        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	              if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(null)) {//用户点击了确定  
+	                  path = fileChooser.getSelectedFile().getAbsolutePath();//取得路径选择  
+	      		assignGenField.setText(path);
+	      	}
+	      }
+	      });
+  	
+	  	assignTembtn.addActionListener(new ActionListener() {
+	
+	    	@Override
+	    	public void actionPerformed(ActionEvent e) {
+	    		// TODO Auto-generated method stub
+	    		String path = null;
+		        JFileChooser fileChooser = new JFileChooser(".");
+	            if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(null)) {//用户点击了确定  
+	                path = fileChooser.getSelectedFile().getAbsolutePath();//取得路径选择  
+	    		assignTemField.setText(path);
+	    	}
+	    }
+	    });
+	  	
+	  	generatebtn.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		Generate g = new Generate()
+        		
+        		
+        		
+        		
+        }
+        });
+  	
+        
+        
+//     panel2 layout
     	JScrollPane spane = new JScrollPane();
         spane.getViewport().add(area);
         setTitle("Automatic Grader");
-        setSize(600, 750);
+        setSize(620, 750);
 //        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        createMenuBar();
-        panel.setLayout(new FlowLayout());
+//        createMenuBar();
+        panel2.setLayout(new FlowLayout());
 
         JButton assignbtn = new JButton("Browse Assignment Path");
         JButton answerbtn = new JButton("Browse Answer File");
@@ -70,29 +153,35 @@ public class UserInterface extends JFrame {
         final JTextField scoreField = new JTextField();
         scoreField.setPreferredSize(new Dimension(320, 20));
         final JTextField scoreset = new JTextField("Score Set, Example: A1:5, D10:10");
+        scoreset.setPreferredSize(new Dimension(200, 20));
         final JTextField roundset = new JTextField("Rounding, Example: A1:0.01, D10:0.05");
+        roundset.setPreferredSize(new Dimension(200, 20));
         final JTextArea result = new JTextArea();
         result.setEditable(false);
-        panel.add(assignbtn);
-        panel.add(assignField);
-        panel.add(answerbtn);
-        panel.add(answerField);
-        panel.add(scorebtn);
-        panel.add(scoreField);
-        panel.add(scoreset);
-        panel.add(roundset);
-        panel.add(runbtn); 
-        panel.add(result);
+        panel2.add(assignbtn);
+        panel2.add(assignField);
+        panel2.add(answerbtn);
+        panel2.add(answerField);
+        panel2.add(scorebtn);
+        panel2.add(scoreField);
+        panel2.add(scoreset);
+        panel2.add(roundset);
+        panel2.add(runbtn); 
+        panel2.add(result);
         JScrollPane scroller = new JScrollPane(result);
         scroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scroller.setPreferredSize( new Dimension( 500, 500 ) );
-        panel.add(scroller);
-        panel.setVisible (true);
+        panel2.add(scroller);
+        panel2.setVisible (true);
         result.setText("Here is the report:\n");
         
+    	panel.add(tabbedPane);
+    	tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    	
+    	
         
-        
-        
+//        panel 2
+    	
         assignbtn.addActionListener(new ActionListener() {
         	
         	@Override
@@ -140,7 +229,8 @@ public class UserInterface extends JFrame {
         runbtn.addActionListener(new ActionListener() {
         	@Override
         	public void actionPerformed(ActionEvent e) {
-
+        		File reportFolder = new File("./report/");
+        		for(File file: reportFolder.listFiles()) file.delete();
         		String assignPath = assignField.getText();
         		String answerPath = answerField.getText();
         		String scorePath = scoreField.getText();
@@ -149,6 +239,7 @@ public class UserInterface extends JFrame {
         		
         		Test fly = new Test();
         		try {
+        			
 					fly.exec(assignPath, answerPath, scorePath, scoreSet, roundSet);
 					result.setPreferredSize( new Dimension( 500, 10000 ) );
 					printReport(result);
@@ -194,62 +285,62 @@ public class UserInterface extends JFrame {
     	
     }
 
-	private void createMenuBar() {
-        
-        JMenuBar menubar = new JMenuBar();
-        
-        ImageIcon iconNew = new ImageIcon("new.png");
-        ImageIcon iconOpen = new ImageIcon("open.png");
-        ImageIcon iconSave = new ImageIcon("save.png");
-        ImageIcon iconExit = new ImageIcon("exit.png");
-
-        JMenu fileMenu = new JMenu("File");
-
-        JMenu impMenu = new JMenu("Import");
-
-        JMenuItem assignMi = new JMenuItem("Import Assignment...");
-        JMenuItem answerMi = new JMenuItem("Import Answer...");
-        JMenuItem scoreMi = new JMenuItem("Import Score...");
-        
-        
-//        String assignPath;
-//        assignMi.addActionListener(new OpenFileAction());
-//        String answerPath = answerMi.addActionListener(new OpenFileAction());
-//        String scorePath = scoreMi.addActionListener(new OpenFileAction());
-
-        
-        impMenu.add(assignMi);
-        impMenu.add(answerMi);
-        impMenu.add(scoreMi);
-        
-        
-
-        JMenuItem newMi = new JMenuItem("New", iconNew);
-        JMenuItem openMi = new JMenuItem("Open", iconOpen);
-        JMenuItem saveMi = new JMenuItem("Save", iconSave);
-
-        JMenuItem exitMi = new JMenuItem("Exit", iconExit);
-        exitMi.setToolTipText("Exit application");
-
-        exitMi.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                System.exit(0);
-            }
-        });
-
-        fileMenu.add(newMi);
-        fileMenu.add(openMi);
-        fileMenu.add(saveMi);
-        fileMenu.addSeparator();
-        fileMenu.add(impMenu);
-        fileMenu.addSeparator();
-        fileMenu.add(exitMi);
-
-        menubar.add(fileMenu);
-
-        setJMenuBar(menubar);        
-    }
+//	private void createMenuBar() {
+//        
+//        JMenuBar menubar = new JMenuBar();
+//        
+//        ImageIcon iconNew = new ImageIcon("new.png");
+//        ImageIcon iconOpen = new ImageIcon("open.png");
+//        ImageIcon iconSave = new ImageIcon("save.png");
+//        ImageIcon iconExit = new ImageIcon("exit.png");
+//
+//        JMenu fileMenu = new JMenu("File");
+//
+//        JMenu impMenu = new JMenu("Import");
+//
+//        JMenuItem assignMi = new JMenuItem("Import Assignment...");
+//        JMenuItem answerMi = new JMenuItem("Import Answer...");
+//        JMenuItem scoreMi = new JMenuItem("Import Score...");
+//        
+//        
+////        String assignPath;
+////        assignMi.addActionListener(new OpenFileAction());
+////        String answerPath = answerMi.addActionListener(new OpenFileAction());
+////        String scorePath = scoreMi.addActionListener(new OpenFileAction());
+//
+//        
+//        impMenu.add(assignMi);
+//        impMenu.add(answerMi);
+//        impMenu.add(scoreMi);
+//        
+//        
+//
+//        JMenuItem newMi = new JMenuItem("New", iconNew);
+//        JMenuItem openMi = new JMenuItem("Open", iconOpen);
+//        JMenuItem saveMi = new JMenuItem("Save", iconSave);
+//
+//        JMenuItem exitMi = new JMenuItem("Exit", iconExit);
+//        exitMi.setToolTipText("Exit application");
+//
+//        exitMi.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent event) {
+//                System.exit(0);
+//            }
+//        });
+//
+//        fileMenu.add(newMi);
+//        fileMenu.add(openMi);
+//        fileMenu.add(saveMi);
+//        fileMenu.addSeparator();
+//        fileMenu.add(impMenu);
+//        fileMenu.addSeparator();
+//        fileMenu.add(exitMi);
+//
+//        menubar.add(fileMenu);
+//
+//        setJMenuBar(menubar);        
+//    }
     
     
     public String readFile(File file) {
@@ -278,5 +369,14 @@ public class UserInterface extends JFrame {
                 ex.setVisible(true);
             }
         });
+    }
+    
+    protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
     }
 }
